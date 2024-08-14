922b7f72b37489b9a0d211ef76c06f235338c7ec
Faizan <rfaizan15@gmail.com>
2024-08-14 16:26:00 +0500

    Basic HTML skeleton, CSS to center that skeleton, and basic functions in script

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let operandA = "";
+let operator = "";
+let operandB = "";
+
+function operate(operator, operandA, operandB) {
+  switch (operator) {
+    case "+":
+      //...
+      break;
+    case "-":
+      //...
+      break;
+    case "*":
+      //...
+      break;
+    case "/":
+      //...
+      break;
+  }
+}
